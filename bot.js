43060a2cb29fb0dcb5d1b620e303d7cdeb9532ad
Oxyyy <petitloulouarceus@gmail.com>
2020-05-03 22:17:01 +0200

    Erick

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const { prefix, token, masterID } = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const cooldowns = new Discord.Collection();
 
